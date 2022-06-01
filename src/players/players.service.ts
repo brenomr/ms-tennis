5924a9c2ef9217bc043ac765cd7b572bcc229a6f
Breno Rodrigues <brenocartoon@hotmail.com>
2022-05-31 22:14:29 -0300

    Add method create update player with mock info

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { CreatePlayerDto } from './dtos/create-player.dto';
+import { IPlayer } from './interfaces/player.interface';
 
 @Injectable()
-export class PlayersService {}
+export class PlayersService {
+  private players: IPlayer[] = [];
+  private readonly logger = new Logger(PlayersService.name);
+
+  async createUpdatePlayer(createPlayerDto: CreatePlayerDto): Promise<void> {
+    await this.create(createPlayerDto);
+  }
+
+  private async create(createPlayerDto: CreatePlayerDto): Promise<void> {
+    const { name, phoneNumber, email } = createPlayerDto;
+
+    const player: IPlayer = {
+      _id: uuid(),
+      name,
+      phoneNumber,
+      email,
+      ranking: 'A',
+      positionRanking: 1,
+      urlPhotoPlayer: 'www.some_mock.som/photo123',
+    };
+
+    this.logger.log(`createPlayerDto: ${JSON.stringify(player)}`);
+
+    this.players.push(player);
+
+    console.log(this.players.length);
+  }
+}
